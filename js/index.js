f42900ebccef1386e6913926e7a18b9c48021b23
Andrés Valencia <120055013+andresvalenciadev@users.noreply.github.com>
2023-06-09 14:19:58 +0000

    First commit

---
+++ js/index.js
@@ -0,0 +1,17 @@
+$.ajax({
+    type: "GET",
+    url: "https://jsonplaceholder.typicode.com/users",
+    dataType: "json",
+    success: function (response) {
+        var tablaHtml = '';
+        $.each(response, function (index, data) { 
+            tablaHtml += '<tr>';
+            tablaHtml += '<td>' + data.id + '</td>';
+            tablaHtml += '<td>' + data.name + '</td>';
+            tablaHtml += '<td>' + data.email + '</td>';
+            tablaHtml += '<td>' + data.address.street + '</td>';
+            tablaHtml += '</tr>';
+        });
+        $('#tablaDatos').html(tablaHtml);
+    }
+});
